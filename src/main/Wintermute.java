9b7176e0cfac8b744a8092c508fa4c8471cb3adf
Dozcar <oscarljungberg88@gmail.com>
2015-11-28 15:23:19 +0100

    Added main class
    blabla byxor, blöja, etc

---
+++ src/main/Wintermute.java
@@ -0,0 +1,9 @@
+package main;
+
+public class Wintermute {
+
+	public static void main(String[] args) {
+		System.out.println("and still he’d see the matrix in his sleep, bright lattices of logic unfolding across that colorless void. . . ");
+	}
+
+}
