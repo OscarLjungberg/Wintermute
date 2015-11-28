c774d250cc3f8273a4a231cdb1197c4d747f2885
metzger76 <horror13@live.se>
2015-11-28 16:03:37 +0100

    added br var added name

--- src/main/Wintermute.java
+++ src/main/Wintermute.java
@@ -1,9 +1,22 @@
 package main;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Wintermute {
 
 	public static void main(String[] args) {
 		System.out.println("and still he’d see the matrix in his sleep, bright lattices of logic unfolding across that colorless void. . . ");
-	}
+		String name = "";
+	       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	       try {
+	        name = br.readLine();
+	       } catch (IOException e) {
+	        // TODO Auto-generated catch block
+	        e.printStackTrace();
+	       }
+	       System.out.println("Hi " + name);
+	}  
 
 }
